5a712ef81d37bc8a854b69d744882f7af9951dde
itsguest <itsguest@d207-023-205-050.wireless.sfu.ca>
2014-12-23 18:54:18 -0800

    fb login works with the login button

--- Study With Me/ViewController.m
+++ Study With Me/ViewController.m
@@ -19,18 +19,9 @@
 
 @implementation ViewController
 
-
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-    
-//    NSString *permissions[2];
-//    permissions[0]=@"public_profile";
-//    permissions[1]=@"email";
+-(IBAction)loginClicked:(id)sender{
     NSArray *permissions = @[@"public_profile",@"email"];
-   
+    
     [PFFacebookUtils logInWithPermissions:permissions block:^(PFUser *user, NSError *error) {
         if (user==nil) {
             NSLog(@"Uh oh. The user cancelled the Facebook login.");
@@ -41,6 +32,23 @@
         }
     }];
 }
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+//    NSArray *permissions = @[@"public_profile",@"email"];
+//   
+//    [PFFacebookUtils logInWithPermissions:permissions block:^(PFUser *user, NSError *error) {
+//        if (user==nil) {
+//            NSLog(@"Uh oh. The user cancelled the Facebook login.");
+//        } else if (user.isNew) {
+//            NSLog(@"User signed up and logged in through Facebook!");
+//        } else {
+//            NSLog(@"User logged in through Facebook!");
+//        }
+//    }];
+}
 
 - (void)didReceiveMemoryWarning
 {
@@ -48,6 +56,7 @@
     // Dispose of any resources that can be recreated.
     
 }
+
 
 
 
